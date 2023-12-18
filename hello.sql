04bd98fb5bf8416ba41203adeb9af69e8597d3b1
meshack254 <meshackmutinda555@gmail.com>
2023-12-18 19:07:51 +0300

    Added trigger for handlign new users

---
+++ hello.sql
@@ -0,0 +1,13 @@
+-- Paste this code in supabase table editor and run it
+create function public.handle_new_user()
+returns trigger as $$
+begin
+  insert into public.user(id, email)
+  values (new.id, new.email);
+  return new;
+end;
+$$ language plpgsql security definer;
+
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
